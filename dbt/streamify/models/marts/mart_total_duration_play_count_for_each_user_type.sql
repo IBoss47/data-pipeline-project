20e878267e0786faab38d484a8d23ed655bca6af
Thanin Tangkoblap <work.thanin@gmail.com>
2026-08-09 10:35:15 +0700

    [feat] Created mart total duration, play count for level category

---
+++ dbt/streamify/models/marts/mart_total_duration_play_count_for_each_user_type.sql
@@ -0,0 +1,17 @@
+with fct_listen_events as (
+    select * from {{ref('fct_streamify__listen_events')}}
+),
+dim_users as (
+    select * from {{ref('dim_streamify__users')}}
+),
+
+select 
+    du.level,
+    sum(fl.duration_minutes) / 60 as total_listen_hour,
+    count(fl.is_valid_play) as total_play
+from fct_listen_events fl
+left join dim_users du 
+using (user_id)
+where fl.is_valid_play = 1
+group by du.level
+order by total_listen_hour, total_play

2aa9d2c8b19bdddc73748ef433ee943bfe141b91
Thanin Tangkoblap <work.thanin@gmail.com>
2026-08-10 15:12:38 +0700

    [feat] Created mart file, that explain performance metrics

---
+++ dbt/streamify/models/marts/mart_content_performance_metrics.sql
@@ -0,0 +1,24 @@
+with fct_listen_events as (
+    select * from {{ref('int_streamify__listen_events')}}
+),
+dim_users as (
+    select * from {{ref('dim_streamify__users')}}
+),
+dim_contents as (
+    select * from {{ref('dim_streamify__contents')}}
+)
+
+select top 10
+    fl.song_id,
+    concat(
+        dc.song_name, ' (', dc.artist_name, ')'
+    ) as content,
+    avg(fl.duration_minutes) as avg_duration,
+    ((count(user_id) - sum(fl.is_valid_play)) * 100.0 / count(fl.song_id)) as skip_rate_pct
+from fct_listen_events fl 
+left join dim_contents dc 
+using (song_id)
+group by song_id, content
+having count(user_id) > 1000
+order by avg_duration desc, skip_rate_pct asc
+
